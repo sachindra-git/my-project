2a21bc7621bbc70abcf5a1e7aabc8eb53bdabadd
sachindra-git <84981563+sachindra-git@users.noreply.github.com>
2025-07-21 11:28:23 +0530

    Update icuRoutes.js

--- routes/icuRoutes.js
+++ routes/icuRoutes.js
@@ -1,1 +1,18 @@
 
+const express = require("express");
+const router = express.Router();
+const {Icu} = require("../models/componentModel");
+
+
+
+// Welcome message for the /components route
+router.get("/", async (req, res) => {
+  try {
+    const icus = await Icu.find();
+    res.json(icus);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
